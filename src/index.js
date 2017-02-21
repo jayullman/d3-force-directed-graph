1f3d000bf53e1c6b5d16d6515cc2e32324098393
Jay Ullman <jayullman@gmail.com>
2017-02-21 00:13:00 -0800

    Update code in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,40 @@
-
 // url from which to receive json data
 const url = 'https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json';
+const minSvgWidth = 310;
+var dataset = [];
 
-import getJson from './getJson';
+import * as d3 from "d3";
+import getJson from './modules/getJson';
+import drawGraph from './modules/drawGraph';
+import getSvgWidth from './modules/getSvgWidth';
 
 // dataset is a promise for the requested json dataset
 let promisedData = getJson(url);
 
-function dataReceived() {
-  console.log('done!');
-}
-
 promisedData
-  .then((dataset) => console.log(dataset))
+  .then((dataset) => {
+    dataset = dataset;
+    drawGraph(dataset);
+  })
   .catch(error => console.log(error));
+
+// create tooltip and append to body, add event listeners for window resizing
+window.onload = function() {
+  var testing;
+  // create tooltip
+  var tooltip = d3.select('body')
+    .append('div')
+    .attr('class', 'tooltip');
+
+  // labels within tooltip that will display the name of the country
+  tooltip.append('div')
+    .attr('class', 'tooltip-info');
+
+  // window.addEventListener('resize', function(event) {
+  //   var svgWidth = getSVGWidth(minSvgWidth);
+  //   // remove old chart and legend box on resizing
+  //   d3.select('svg').remove();
+  //   d3.select('.legend').remove();
+  //   drawGraph(dataset);
+  // });
+};   
